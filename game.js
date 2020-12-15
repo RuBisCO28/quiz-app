59623daf38ad23a6f8715d6e93592f84d8c70240
RuBisCO28 <54470624+RuBisCO28@users.noreply.github.com>
2020-12-15 22:10:03 +0900

    Change quiz type

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progreeText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progreeBarFull = document.querySelector('#progreeBarFull');
@@ -9,96 +8,56 @@ let acceptAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-
-let questions = [
-    {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2
-    },
-    {
-        question: 'What is 1 + 1?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 1
-    },
-    {
-        question: 'What is 20 + 1?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 3
-    },
-    {
-        question: 'What is 12 + 5?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 4
-    }
-];
+let maxQuestions = 0;
+let param = "";
 
 const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 4;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
+    param = location.search.substring(4,5);
+    availableQuestions = questions[param];
+    maxQuestions = availableQuestions.sentence.length;
     getNewQuestion();
 };
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    if(availableQuestions.sentence.length === 0 || questionCounter > maxQuestions) {
         localStorage.setItem('mostRecentScore', score);
         return window.location.assign('./end.html');
     }
     questionCounter++;
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
+    progressText.innerText = `Question ${questionCounter} of ${maxQuestions}`;
+    progressBarFull.style.width = `${(questionCounter/maxQuestions) * 100}%`;
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex];
-    question.innerText = currentQuestion.question;
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.sentence.length);
+    currentQuestion = availableQuestions.sentence[questionsIndex];
 
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
+    let audio = document.createElement('audio');
+    audio.src = currentQuestion.question;
+    audio.controls = true;
+    question.appendChild(audio);
 
-    availableQuestions.splice(questionsIndex, 1);
+    availableQuestions.sentence.splice(questionsIndex, 1);
     acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+checkAnswer = () => {
+    const myAnswer = document.getElementById("msheet").value;
+    let judge = myAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+    if(judge === 'correct') {
+        incrementScore(SCORE_POINTS);
+    }
+    document.getElementById("asheet").value = judge + '\n' + currentQuestion.answer + '\n' + currentQuestion.jp;
+}
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS);
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 500);
-
-    });
-});
+nextQuestion = () => {
+    getNewQuestion();
+    question.removeChild(question.firstChild);
+    document.getElementById("msheet").value = "";
+    document.getElementById("asheet").value = "";
+}
 
 incrementScore = num => {
     score += num;
